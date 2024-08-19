a027afc000df40e96d152ed0e0036f7f34a89389
mithileshbadge2 <mithileshbadge2@gmail.com>
2024-08-19 17:49:32 +0530

    added

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,13 +5,15 @@ import App from './App';
 import App3 from './App3';
 import App4 from './App4';
 import App5 from './App5';
+import App6 from './App6';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App3 /> */}
     {/* <App4/> */}
-    <App5/>
+    {/* <App5/> */}
+    <App6/>
   </React.StrictMode>
 );
 
